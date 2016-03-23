5182c0e0d7290b66c4ef240573017b7e04471d78
hiskiayansen <hiskiayansen@gmail.com>
2016-03-23 12:31:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 
-class mahasiswa{
-var $Nama;
-
-//methode
-function SetNama($parameter)
-$this->Nama=$parameter;
-}
-function GetNama(){
-echo $this->Nama. "<br/>";
-
-}}
-
-$Yansen = new mahasiswa;
-$Yansen->SetNama("Nama saya Yansen");
-$Yansen->GetNama();
+echo "Hallo saya yansen";
 
 ?>
