3b92943488f4025b48017c779851a470e1751674
hiskiayansen <hiskiayansen@gmail.com>
2016-03-23 12:22:05 +0700

    Added files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+class mahasiswa{
+var $Nama;
+
+//methode
+function SetNama($parameter)
+$this->Nama=$parameter;
+}
+function GetNama(){
+echo $this->Nama. <br/>";
+
+}}
+
+$Yansen = new mahasiswa;
+$Yansen->SetNama("Nama saya Yansen");
+$Yansen->GetNama();
+
+?>
